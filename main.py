09b84eae72b0acbb827d2986bddbe83d1a7b6237
NehaShrivastava-Tech <neha.shrivastava2702@gmail.com>
2026-01-13 20:04:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+#Expense Tracker Project
+
+expensesList = []  #list of expenses on form of dictonary
+print(" Welcome to Expenses Tracker : ")
+
+while True:
+    print("====MENU====")
+    print("1. Add Expenses")
+    print("2. View All Expenses")
+    print("3. View Total Expenses")
+    print("4. Exit")
+
+    choice= int(input("Please Enter Your Choice : "))
+
+# ADD Expense 
+    if(choice == 1):
+        date= input("Enter the date: ")
+        category= input("Enter the category(Food, travel, Books): ")
+        description= input("Enter details: ")
+        amount= float(input("Enter the amount: "))
+
+        expense= {
+            "date":  date,
+            "category":  category,
+            "description":  description,
+            "amount": amount
+        }
+
+        expensesList.append(expense)
+        print(" \n DONE bro. Expense is added succesfully")
+
+# 2.  VIEW ALL EXPENSES
+    elif(choice == 2):
+        if( len(expensesList)==0 ):
+            print("NO Expenses Added. ")
+        else:
+            print("====== Your All Expense ======")
+            count= 1
+            for eachexpense in expensesList:
+                print(f"expense Number {count} -> {eachexpense["date"]}, {eachexpense["category"]}, {eachexpense["description"]}, {eachexpense["amount"]}")  
+                count= count+1
+
+# 3.  VIEW TOTAL SPENDING
+    elif(choice == 3):
+        total= 0
+        for eachexpense in expensesList:
+            total = total + eachexpense["amount"]
+
+            print("\n TOTAL EXPENSE = ", total)
+
+# 4.  EXIT
+    elif(choice == 4):
+        print("Thank you for using our system")
+        break
+
+    else:
+        print("INVALID CHOICE, TRY AGAIN")
+
+
+                                             
+
+             
+         
+
+
+
